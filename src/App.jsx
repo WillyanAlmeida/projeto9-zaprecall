9b03e9cb978ece2f897cbd7009a72f7e2315cc6e
Willyan <wkinhaamaral@gmail.com>
2023-05-22 13:12:19 -0300

    correcao link img

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import GlobalStyle from './globalStyles.js'
 import Welcome from './Welcome.jsx'
 import Zaprecall from './Zaprecall.jsx'
 const img = {
-  logo:"./src/assets/logo.png", 
-  play:"./src/assets/seta_play.png",
-  virar:"./src/assets/seta_virar.png",
-  certo:"./src/assets/icone_certo.png",
-  erro:"./src/assets/icone_erro.png",
-  quase:"./src/assets/icone_quase.png",
-  party:"./src/assets/party.png",
-  sad:"./src/assets/sad.png"
+  logo:"/src/assets/logo.png", 
+  play:"/src/assets/seta_play.png",
+  virar:"/src/assets/seta_virar.png",
+  certo:"/src/assets/icone_certo.png",
+  erro:"/src/assets/icone_erro.png",
+  quase:"/src/assets/icone_quase.png",
+  party:"/src/assets/party.png",
+  sad:"/src/assets/sad.png"
 }
 
 function App() {
